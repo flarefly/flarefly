03c39fc0b07f3ae5ef65d84d26d94cd13196afdf
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2022-08-02 14:10:51 +0200

    Avoid duplication of code

--- tests/test_data_handler.py
+++ tests/test_data_handler.py
@@ -7,11 +7,8 @@ import numpy as np
 import pandas as pd
 from flarefly.data_handler import DataHandler
 
-LIMITS = [1.75, 2.0]
-DATASGN = np.random.normal(1.865, 0.010, size=10000)
-DATABKG = np.random.uniform(LIMITS[0], LIMITS[1], size=10000)
-DATA = DataHandler(np.concatenate((DATASGN, DATABKG), axis=0),
-                   var_name=r'$M$ (GeV/$c^{2}$)', limits=LIMITS)
+DATANP = np.random.normal(0, 1, size=10000)
+DATA = DataHandler(DATANP, axis=0, var_name='x', limits=[1.75, 2.0])
 
 def test_data():
     """
