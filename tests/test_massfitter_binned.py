56cf8b5baacf39dcf4040e4417f4579c93600fd3
Fabrizio <fabrizio.grosa@cern.ch>
2022-08-29 10:02:48 +0200

    Refactory fitter class to handle list of signal and bkg pdfs (#25)
    * Refactory fitter class to handle list of signal and bkg pdfs

--- tests/test_massfitter_binned.py
+++ tests/test_massfitter_binned.py
@@ -14,14 +14,20 @@ INFILE = os.path.join(os.getcwd(), "tests/histos.root")
 DATABINNED = DataHandler(INFILE, var_name=r'$M_\mathrm{K\pi\pi}$ (GeV/$c^{2}$)',
                          histoname='hMass_80_120', limits=[1.75, 2.06])
 
-FITTERBINNED = F2MassFitter(DATABINNED, name_signal_pdf='gaussian', name_background_pdf='expo')
-FITTERBINNED.set_particle_mass(1.87)
-FITTERBINNED.set_secpeak("gaussian", 2.01, 0.01)
+FITTERBINNED = F2MassFitter(DATABINNED,
+                            name_signal_pdf=['gaussian', 'gaussian'],
+                            name_background_pdf=['expo'])
+FITTERBINNED.set_particle_mass(0, 1.87)
+FITTERBINNED.set_particle_mass(1, 2.01)
+FITTERBINNED.set_signal_initpar(0, "sigma", 0.010)
+FITTERBINNED.set_signal_initpar(1, "sigma", 0.015)
 FITRES = FITTERBINNED.mass_zfit()
 FIG = FITTERBINNED.plot_mass_fit('ATLAS')
 
 RAWYHIST = uproot.open(INFILE)["hRawYields"].to_numpy()
 RAWYIN = RAWYHIST[0][4]
+
+matplotlib.pyplot.show()
 
 def test_fitter():
     """
