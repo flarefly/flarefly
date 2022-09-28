c7bddac4db2ffea7938d912b631c2ecf3af5ced1
stefanopolitano <stefano.politano@cern.ch>
2022-09-28 19:35:08 +0200

    Adding test for chebpolN fit; Moving chebpol definition; Adjusting docstring

--- tests/test_massfitter_binned.py
+++ tests/test_massfitter_binned.py
@@ -14,18 +14,27 @@ INFILE = os.path.join(os.getcwd(), 'tests/histos.root')
 DATABINNED = DataHandler(INFILE, var_name=r'$M_\mathrm{K\pi\pi}$ (GeV/$c^{2}$)',
                          histoname='hMass_80_120', limits=[1.75, 2.06])
 
-FITTERBINNED = F2MassFitter(DATABINNED,
-                            name_signal_pdf=['gaussian', 'gaussian'],
-                            name_background_pdf=['expo'])
-FITTERBINNED.set_particle_mass(0, mass=1.872, fix=True)
-FITTERBINNED.set_particle_mass(1, pdg_id=413, limits=[2.000, 2.020])
-FITTERBINNED.set_signal_initpar(0, 'sigma', 0.010)
-FITTERBINNED.set_signal_initpar(1, 'sigma', 0.015, limits=[0.01, 0.03])
-FITRES = FITTERBINNED.mass_zfit()
-FIG = FITTERBINNED.plot_mass_fit(style='ATLAS')
 
-RAWYHIST = uproot.open(INFILE)["hRawYields"].to_numpy()
-RAWYIN = RAWYHIST[0][4]
+bkg_pdfs = ['chebpol1', 'expo']
+FITTERBINNED, FITRES, FIG, RAWYHIST, RAWYIN = ([] for _ in range(5))
+for ibkg, bkg_pdf in enumerate(bkg_pdfs):
+    FITTERBINNED.append(F2MassFitter(DATABINNED,
+                                     name_signal_pdf=['gaussian', 'gaussian'],
+                                     name_background_pdf=[bkg_pdf],
+                                     name=f"{bkg_pdf}"))
+    FITTERBINNED[ibkg].set_particle_mass(0, mass=1.872, fix=True)
+    FITTERBINNED[ibkg].set_particle_mass(1, pdg_id=413, limits=[2.000, 2.020])
+    FITTERBINNED[ibkg].set_signal_initpar(0, 'sigma', 0.010)
+    FITTERBINNED[ibkg].set_signal_initpar(1, 'sigma', 0.015, limits=[0.01, 0.03])
+    if bkg_pdf == 'chebpol1':
+        FITTERBINNED[ibkg].set_background_initpar(0, 'c0', 850)
+        FITTERBINNED[ibkg].set_background_initpar(0, 'c1', -0.4)
+    elif 'chebpol' in bkg_pdf:
+        print("WARNING: Chebyshev polynomials require intial parameters to be set.")
+    FITRES.append(FITTERBINNED[ibkg].mass_zfit())
+    FIG.append(FITTERBINNED[ibkg].plot_mass_fit(style='ATLAS'))
+    RAWYHIST.append(uproot.open(INFILE)["hRawYields"].to_numpy())
+    RAWYIN.append(RAWYHIST[ibkg][0][4])
 
 def test_fitter():
     """
