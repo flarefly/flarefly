90283defd4bf208b7bfa5fab6394cdd7a94109a8
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2023-01-10 09:02:59 +0100

    Add python3.10

--- setup.py
+++ setup.py
@@ -71,7 +71,8 @@ SETUP = Setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9"
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
     ],
 
     # What does your project relate to?
