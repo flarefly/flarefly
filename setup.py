bf4231904cc2eb348b1b8a47b426a73ddd6693e7
Fabrizio Grosa <fabrizio.grosa@cern.ch>
2025-06-04 18:20:28 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ SETUP = Setup(
     name="flarefly",
 
     # LAST-TAG is a placeholder. Automatically replaced at deploy time with the right tag
-    version="0.0.13",
+    version="0.0.15",
     description="FLexible And REliable Fitting LibrarY for particle physics analysis",
     url="https://github.com/flarefly/flarefly",
     author="flarefly-developers",
