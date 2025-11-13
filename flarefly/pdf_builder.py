750129208b083dc2e9c5ffe2bfe928b3a622ab1d
fchinu <fachi2001@gmail.com>
2025-11-13 16:49:20 +0100

    Add properties, setters, representations in pdf base

--- flarefly/pdf_builder.py
+++ flarefly/pdf_builder.py
@@ -29,7 +29,6 @@ class PDFBuilder:
             name: Base name for parameters
             ipdf: Index of the PDF
         """
-        pdf.print()
         if pdf.is_kde():
             pdf = PDFBuilder.build_signal_kde(
                 pdf,
@@ -70,17 +69,17 @@ class PDFBuilder:
                 param_key = f'{name}_{arg_name}_signal{ipdf}'
             pdf_args[arg_name] = parameters[param_key]
 
-        pdf.set_pdf(config['pdf_class'](**pdf_args))
+        pdf.pdf = config['pdf_class'](**pdf_args)
         pdf.parameters = parameters
 
         if pdf.at_threshold:
             # pion mass as default
-            pdf.set_default_init_par('powerthr', 1.)
-            pdf.set_default_init_par('massthr', pdf.pdg_api.get_particle_by_mcid(211).mass)
-            pdf.set_default_limits_par('massthr', [None, None])
-            pdf.set_default_limits_par('powerthr', [None, None])
-            pdf.set_default_fix_par('powerthr', False)
-            pdf.set_default_fix_par('massthr', True)
+            pdf.set_default_par(
+                'massthr',
+                init=pdf.pdg_api.get_particle_by_mcid(211).mass,
+                fix=True, limits=[None, None]
+            )
+            pdf.set_default_par('powerthr', init=1., fix=False, limits=[None, None])
             pdf.parameters[f'{name}_massthr_signal{ipdf}'] = zfit.Parameter(
                 f'{name}_massthr_signal{ipdf}', pdf.get_init_par('massthr'),
                 pdf.get_limits_par('massthr')[0], pdf.get_limits_par('massthr')[1],
@@ -94,8 +93,7 @@ class PDFBuilder:
                 mass=pdf.parameters[f'{name}_massthr_signal{ipdf}'],
                 power=pdf.parameters[f'{name}_powerthr_signal{ipdf}']
             )
-            pdf.set_pdf(zfit.pdf.ProductPDF(
-                [pdf.get_pdf(), signalthr_pdf], obs=obs))
+            pdf.pdf = zfit.pdf.ProductPDF([pdf.pdf, signalthr_pdf], obs=obs)
 
     @staticmethod
     def build_signal_kde(
@@ -115,12 +113,12 @@ class PDFBuilder:
 
         kde_options = pdf.kde_option or {}
 
-        pdf.set_pdf(get_kde_pdf(pdf.kind)(
+        pdf.pdf = get_kde_pdf(pdf.kind)(
             data=pdf.kde_sample,
             obs=pdf.kde_sample.get_obs(),
             name=f'{name}_kde_signal{ipdf}',
             **kde_options
-        ))
+        )
 
     @staticmethod
     def build_signal_hist(
@@ -143,14 +141,14 @@ class PDFBuilder:
         if not pdf.hist_sample:
             Logger(f'Missing datasample for histogram template of signal {ipdf}!', 'FATAL')
 
-        pdf.set_pdf(zfit.pdf.SplinePDF(
+        pdf.pdf = zfit.pdf.SplinePDF(
             zfit.pdf.HistogramPDF(
                 pdf.hist_sample.get_binned_data(),
                 name=f'{name}_hist_signal{ipdf}'
             ),
             order=3,
             obs=obs
-        ))
+        )
 
     @staticmethod
     def build_bkg_pdf(
@@ -215,7 +213,7 @@ class PDFBuilder:
                 param_key = f'{name}_{arg_name}_bkg{ipdf}'
             pdf_args[arg_name] = parameters[param_key]
 
-        pdf.set_pdf(config['pdf_class'](**pdf_args))
+        pdf.pdf = config['pdf_class'](**pdf_args)
         pdf.parameters = parameters
 
     @staticmethod
@@ -250,7 +248,7 @@ class PDFBuilder:
 
 
         print("Parameters: chebyyy ", parameters)
-        pdf.set_pdf(zfit.pdf.Chebyshev(obs=obs, coeff0=coeff0, coeffs=bkg_coeffs))
+        pdf.pdf = zfit.pdf.Chebyshev(obs=obs, coeff0=coeff0, coeffs=bkg_coeffs)
         pdf.parameters = parameters
 
         print(pdf.parameters)
@@ -279,12 +277,12 @@ class PDFBuilder:
         kde_options = pdf.kde_options or {}
 
 
-        pdf.set_pdf(get_kde_pdf(pdf.kind)(
+        pdf.pdf = get_kde_pdf(pdf.kind)(
             data=pdf.kde_sample,
             obs=pdf.kde_sample.get_obs(),
             name=f'{name}_kde_bkg{ipdf}',
             **kde_options
-        ))
+        )
 
     @staticmethod
     def build_bkg_hist(
@@ -304,14 +302,14 @@ class PDFBuilder:
         if not pdf.hist_bkg_sample:
             Logger(f'Missing datasample for histogram template of background {ipdf}!', 'FATAL')
 
-        pdf.set_pdf(zfit.pdf.SplinePDF(
+        pdf.pdf = zfit.pdf.SplinePDF(
             zfit.pdf.HistogramPDF(
                 pdf.hist_bkg_sample.get_binned_data(),
                 name=f'{name}_hist_bkg{ipdf}'
             ),
             order=3,
             obs=obs
-        ))
+        )
 
     @staticmethod
     def _update_with_defaults(
@@ -319,10 +317,7 @@ class PDFBuilder:
         pdf: F2PDFBase
     ) -> None:
         """Update the parameter dictionaries with default values from config."""
-        init_pars = pdf.get_init_pars()
-        limits_pars = pdf.get_limits_pars()
-        fix_pars = pdf.get_fix_pars()
         for par_name, par_config in config['parameters'].items():
-            init_pars.setdefault(par_name, par_config['init'])
-            limits_pars.setdefault(par_name, par_config['limits'])
-            fix_pars.setdefault(par_name, par_config['fix'])
+            pdf.set_default_init_par(par_name, par_config['init'])
+            pdf.set_default_limits_par(par_name, par_config['limits'])
+            pdf.set_default_fix_par(par_name, par_config['fix'])
