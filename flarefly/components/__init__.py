b2a4bebdd6be50de7d4a6a2aa04e1fd8ebb77bfe
fchinu <fachi2001@gmail.com>
2025-12-04 16:58:40 +0100

    pylint

--- flarefly/components/__init__.py
+++ flarefly/components/__init__.py
@@ -7,4 +7,4 @@ F2PDFBase is the base class for all PDF implementations
 from .pdf_kind import PDFKind, PDFType
 from .pdf_base import F2PDFBase
 
-__all__ = ["PDFKind", "PDFType", "F2PDFBase", "F2ComposedPDF"]
+__all__ = ["PDFKind", "PDFType", "F2PDFBase"]
