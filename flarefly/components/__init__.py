b40866827c3bcb602a8f8d872ab227a09cad12a2
fchinu <fachi2001@gmail.com>
2025-12-04 13:28:40 +0100

    Fitter refactor: create composed PDF

--- flarefly/components/__init__.py
+++ flarefly/components/__init__.py
@@ -6,5 +6,6 @@ F2PDFBase is the base class for all PDF implementations
 """
 from .pdf_kind import PDFKind, PDFType
 from .pdf_base import F2PDFBase
+from .composed_pdf import F2ComposedPDF
 
-__all__ = ["PDFKind", "PDFType", "F2PDFBase"]
+__all__ = ["PDFKind", "PDFType", "F2PDFBase", "F2ComposedPDF"]
